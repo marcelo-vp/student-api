bee2bde989c604549bcc88931f0b1369915dd74c
marcelo-vp <marcelos.mail@gmail.com>
2020-07-12 14:35:15 -0300

    Remove addresses table

--- student_api/common/database.py
+++ student_api/common/database.py
@@ -21,26 +21,18 @@ class Student(Base):
     __tablename__  = 'students'
 
     id = Column(Integer, primary_key=True)
-    first_name = Column(String(250), nullable=False)
-    last_name = Column(String(250), nullable=False)
+    first_name = Column(String(100), nullable=False)
+    last_name = Column(String(100), nullable=False)
     age = Column(Integer, nullable=False)
-    responsible_adult = Column(String(250), nullable=False)
-    school_grade = Column(String(250))
+    responsible_adult = Column(String(100), nullable=False)
+    school_grade = Column(String(100))
     discipline_grades = Column(JSON)
     last_registration = Column(DateTime)
-    late_payments = Column(Integer)
-
-
-class Address(Base):
-    __tablename__ = 'addresses'
-
-    id = Column(Integer, primary_key=True)
-    zip_code = Column(String(8))
-    street_name = Column(String(250))
-    street_number = Column(Integer)
-    complement = Column(String(250))
-    student = relationship(Student)
-    student_id = Column(Integer, ForeignKey('students.id'))
+    late_payments = Column(Integer, default=0)
+    zip_code = Column(String(8), nullable=False)
+    street_name = Column(String(200), nullable=False)
+    street_number = Column(Integer, nullable=False)
+    complement = Column(String(200))
 
 
 db_connector = Connector()
